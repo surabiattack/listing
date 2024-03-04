7a5a89b45eecf89bcf15d069cd3362c7cffcb8ff
Dwi Kuntarto <dwikuntarto.work@gmail.com>
2024-03-04 21:34:13 +0700

    4. 4_Admin Profile - Profile From Fields (Part - 2)

--- resources/views/admin/profile/index.blade.php
+++ resources/views/admin/profile/index.blade.php
@@ -29,7 +29,7 @@
                                         <label>Avatar</label>
                                         <div id="image-preview" class="image-preview">
                                             <label for="image-upload" id="image-label">Choose File</label>
-                                            <input type="file" name="image" id="image-upload" />
+                                            <input type="file" name="avatar" id="image-upload" />
                                         </div>
                                     </div>
                                 </div>
@@ -38,8 +38,84 @@
                                         <label>Banner</label>
                                         <div id="image-preview-2" class="image-preview">
                                             <label for="image-upload-2" id="image-label-2">Choose File</label>
-                                            <input type="file" name="image" id="image-upload-2" />
+                                            <input type="file" name="banner" id="image-upload-2" />
                                         </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label>Name<span class="text-danger">*</span></label>
+                                        <input type="text" class="form-control" name="name" required />
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label>Email<span class="text-danger">*</span></label>
+                                        <input type="text" class="form-control" name="email" required />
+                                    </div>
+                                </div>
+
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label>Phone<span class="text-danger">*</span></label>
+                                        <input type="text" class="form-control" name="phone" required />
+                                    </div>
+                                </div>
+
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label>Address<span class="text-danger">*</span></label>
+                                        <input type="text" class="form-control" name="address" required />
+                                    </div>
+                                </div>
+
+                                <div class="col-md-12">
+                                    <div class="form-group">
+                                        <label>About<span class="text-danger">*</span></label>
+                                        <textarea name="about" class="form-control" required></textarea>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label>Website</label>
+                                        <input type="text" class="form-control" name="website" />
+                                    </div>
+                                </div>
+
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label>Facebook</label>
+                                        <input type="text" class="form-control" name="fb_link" />
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label>X</label>
+                                        <input type="text" class="form-control" name="x_link" />
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label>Linkedin</label>
+                                        <input type="text" class="form-control" name="in_link" />
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label>Whatsapp</label>
+                                        <input type="text" class="form-control" name="wa_link" />
+                                    </div>
+                                </div>
+
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label>Instagram</label>
+                                        <input type="text" class="form-control" name="instra_link" />
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <button type="submit" class="btn btn-primary">Update</button>
                                     </div>
                                 </div>
 
